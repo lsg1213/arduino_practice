de22e32f84bb39b86eb3749c15d0f44d780a9dd9
Seungjin Lee <lsg0385@naver.com>
2019-10-09 23:55:12 +0900

    homework3

--- homework3/homework3.ino
+++ homework3/homework3.ino
@@ -1,17 +1,24 @@
 #include <MsTimer2.h> // 타이머 인터럽트 라이브러리 헤더파일
 
+// variable
+int velocity = 0;
+bool direct = false;  // true: 정회전, false: 역회전
 void setup() {
   Serial.begin(9600);
   pinMode(2,OUTPUT);  //센서 TRIG 핀
   pinMode(3,INPUT);   //센서 ECHO 핀
-  pinMode(12, OUTPUT);
-
   MsTimer2::set(500, sensing);
   // 타이머 인터럽트 설정(500ms 마다 flash 함수를 호출)
   MsTimer2::start(); // 타이머 인터럽트 실행
+  attachInterrupt(5, rotationChange, RISING);
 }
+
 int microsecondsToCentimeters(int microseconds){
   return (double)microseconds / 29.0 / 2.0;
+}
+
+void rotationChange() {
+  direct = !direct;
 }
 
 void sensing() // 인터럽트 발생 시 동작할 함수 
@@ -26,14 +33,21 @@ void sensing() // 인터럽트 발생 시 동작할 함수
   //ECHO pin : HIGH-> LOW 간격 측정
   cm = microsecondsToCentimeters(duration);  
   //거리(cm)로 변환
-  
+  if (cm <= 10) {
+    velocity = 120;
+  } else {
+    velocity = 255;
+  }
   Serial.print(cm);
   Serial.println("cm");
 }
 
 void loop() {
-  digitalWrite(12, HIGH);
-  delay(500);
-  digitalWrite(12, LOW);  
-  delay(500);
+  if (direct) {
+    analogWrite(8, velocity);
+    analogWrite(11, 0);
+  } else {
+    analogWrite(8, 0);
+    analogWrite(11, velocity);
+  }
 }
